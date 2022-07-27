ee395efed02be5e32129147ba98edc00ae277363
muzammilshaikh12 <110051885+muzammilshaikh12@users.noreply.github.com>
2022-07-27 21:53:24 +0530

    Get Elements by tag name updated

---
+++ dom.js
@@ -0,0 +1,24 @@
+ const a = document.getElementById('header-title');
+ a.innerHTML="Non Item Lister"
+// const b=document.getElementById('main-header')
+// b.style.borderBottom="3px solid black"
+
+// const c= document.querySelector(".title")
+// c.style.color="green"
+
+// Get elements by class name
+// const items=document.getElementsByClassName('list-group-item')
+// items[2].style.backgroundColor="green";
+// for(let i=0;i<items.length;i++) {
+//     items[i].style.fontWeight='bold'
+// }
+
+ const li=document.getElementsByTagName('li');
+ for(let i=0;i<=li.length;i++) {
+    li[i].style.color="blue";
+ }
+ 
+
+
+
+ 
